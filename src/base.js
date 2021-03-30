749ce8d7d838b9a82323b4e004e8a052d6e729ca
GabrielDamian <76115929+GabrielDamian@users.noreply.github.com>
2021-03-30 22:48:15 +0300

    deploy

---
+++ src/base.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyAoNFRYdATxCIM04OHJk7OglN06GmyznGQ",
+    authDomain: "message-app-9e405.firebaseapp.com",
+    projectId: "message-app-9e405",
+    storageBucket: "message-app-9e405.appspot.com",
+    messagingSenderId: "877171844654",
+    appId: "1:877171844654:web:0dbd95f638974bb68361bf",
+    measurementId: "G-55NPY01XTB"
+})
+
+export default app;
